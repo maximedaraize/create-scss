b3a9e2f4422b12dd3f4890c154deab239fa707cd
maximedaraize <maxime.daraize@gmail.com>
2020-04-10 08:18:19 -0400

    fix important typo in index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fs = require("fs-extra");
 // Async/Await:
 async function copyFiles() {
   try {
-    await fs.copy("scss", "../../sass");
+    await fs.copy("scss", "../../scss");
     console.log("success!");
   } catch (err) {
     console.error(err);
