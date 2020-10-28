e500944c755ef63407726fce55c9d2c4db54b496
Maxime Daraize <leeroydevassistant2@gmail.com>
2020-10-27 20:38:08 -0400

    initialize new scripts

--- postinstall.js
+++ postinstall.js
@@ -39,11 +39,9 @@ if (!json.hasOwnProperty('scripts')) {
   json.scripts = {};
 }
 
-json.scripts['cs-watch'] = 'sass scss/main.scss css/style.css --watch';
-json.scripts['cs-compile'] = 'sass scss/main.scss css/style.comp.css';
-json.scripts['cs-prefix'] =
-  "postcss css/style.comp.css --use autoprefixer -b 'last 4 versions' -o css/style.prefix.css";
-json.scripts['cs-compress'] =
-  'sass css/style.prefix.css css/style.mini.css --output-style compressed';
-json.scripts['cs-build'] = 'npm-run-all cs-compile cs-prefix cs-compress';
+json.scripts['cs-watch'] = 'sass scss/main.scss css/style.css --watch --no-source-map';
+json.scripts['cs-compile'] = 'sass scss/main.scss css/style.css --no-source-map';
+json.scripts['cs-compress'] = 'sass scss/main.scss css/style.css --style=compressed --no-source-map';
+json.scripts['cs-prefix'] = "postcss css/style.css -o css/style.css --use autoprefixer -b 'last 4 versions' --no-source-map";
+json.scripts['cs-build'] = 'npm-run-all cs-compile cs-compress cs-prefix';
 saveFile(pkgJsonPath, JSON.stringify(json, null, 2));
