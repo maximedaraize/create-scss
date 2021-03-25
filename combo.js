9daa173140bfc61565af1e16e861cca35c927032
Maxime Daraize <leeroydevassistant2@gmail.com>
2021-03-24 20:19:17 -0400

    remove one line that contain a word

--- combo.js
+++ combo.js
@@ -1,6 +1,5 @@
 const inquirer = require("inquirer");
 const fs = require("fs-extra");
-const path = require("path");
 const glob = require("glob");
 
 let ascii = `
@@ -18,42 +17,46 @@ inquirer
     {
       name: "scss_path",
       type: "input",
-      message: "Where would you like to add the scss directory? (Press enter for root)",
+      message:
+        "Where would you like to add the scss directory? (Press enter for root)",
       default: "./",
     },
     {
-        name: "scss_structure",
-        type: "list",
-        message: "What structure would you like?",
-        choices: ["default", "blank", "custom"],
+      name: "scss_structure",
+      type: "list",
+      message: "What structure would you like?",
+      choices: ["default", "blank", "custom"],
+    },
+    {
+      name: "folder",
+      type: "checkbox",
+      message: "Which folder would you like to remove",
+      choices: [
+        "abstracts",
+        "base",
+        "components",
+        "layout",
+        "pages",
+        "themes",
+        "vendor",
+      ],
+      when: function (answer) {
+        // Only run if user answered "custom" to the first prompt
+        return answer.scss_structure === "custom";
       },
-      {
-        name: "folder",
-        type: "checkbox",
-        message: "Which folder would you like to remove",
-        choices: [
-          "abstracts",
-          "base",
-          "components",
-          "layout",
-          "pages",
-          "themes",
-          "vendor",
-        ],
-        when: function (answer) {
-          // Only run if user answered "custom" to the first prompt
-          return answer.scss_structure === "custom";
-        },
-      },
+    },
   ])
   .then((answer) => {
     async function copyFiles() {
       try {
-        await 
-        fs.copy("node_modules/create-scss/scss", `${answer.scss_path}/scss`, {
-          overwrite: false,
-          errorOnExist: true,
-        });
+        await fs.copy(
+          "node_modules/create-scss/scss",
+          `${answer.scss_path}/scss`,
+          {
+            overwrite: false,
+            errorOnExist: true,
+          }
+        );
         console.log(
           "\x1b[32m",
           "Awesome! A new scss folder was added to your project. You are now ready to code something beautiful"
@@ -69,9 +72,38 @@ inquirer
           let folders = answer.folder;
           folders.forEach(function (item) {
             fs.rm(answer.scss_path + "/scss/" + item, { recursive: true });
+            fs.readFile(
+              "text.txt",
+              { encoding: "utf-8" },
+              function (err, data) {
+                if (err) throw error;
+
+                let dataArray = data.split("\n"); // convert file data in an array
+                const searchKeyword = "base"; // we are looking for a line, contains, key word 'user1' in the file
+                let lastIndex = -1; // let say, we have not found the keyword
+
+                for (let index = 0; index < dataArray.length; index++) {
+                  if (dataArray[index].includes(searchKeyword)) {
+                    // check if a line contains the 'user1' keyword
+                    lastIndex = index; // found a line includes a 'user1' keyword
+                    break;
+                  }
+                }
+
+                dataArray.splice(lastIndex, 1); // remove the keyword 'user1' from the data Array
+
+                // UPDATE FILE WITH NEW DATA
+                // IN CASE YOU WANT TO UPDATE THE CONTENT IN YOUR FILE
+                // THIS WILL REMOVE THE LINE CONTAINS 'user1' IN YOUR shuffle.txt FILE
+                const updatedData = dataArray.join("\n");
+                fs.writeFile("text.txt", updatedData, (err) => {
+                  if (err) throw err;
+                  console.log("Successfully updated the file data");
+                });
+              }
+            );
           });
-          console.log(folders)
-        }        
+        }
         return true;
       } catch (error) {
         if (error.message.includes("already exists")) {
@@ -89,7 +121,8 @@ inquirer
       ascii,
       "\x1b[37m",
       ` \n 🎉 Thank you for using create-scss \n 🌎 Website:`,
-      "\x1b[36m", `https://create-scss.com`
+      "\x1b[36m",
+      `https://create-scss.com`
     );
-      copyFiles();
+    copyFiles();
   });
