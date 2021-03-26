dfe6403ba118b9a566189f95273c8d5dac27e24a
Maxime Daraize <leeroydevassistant2@gmail.com>
2021-03-25 20:40:50 -0400

    make path of compilation dynamic in package.json base on user input + can delete all word from file if are the same

--- combo.js
+++ combo.js
@@ -79,27 +79,35 @@ inquirer
                 if (err) throw error;
 
                 let dataArray = data.split("\n"); // convert file data in an array
-                const searchKeyword = "base"; // we are looking for a line, contains, key word 'user1' in the file
-                let lastIndex = -1; // let say, we have not found the keyword
+                let searchKeyword = Object.values(answer.folder); // we are looking for a line, contains, key word 'user1' in the file
+                // let lastIndex = -1; // let say, we have not found the keyword
 
-                for (let index = 0; index < dataArray.length; index++) {
-                  if (dataArray[index].includes(searchKeyword)) {
-                    // check if a line contains the 'user1' keyword
-                    lastIndex = index; // found a line includes a 'user1' keyword
-                    break;
-                  }
-                }
+                let result = dataArray.filter(function (el) {
+                  return !searchKeyword.includes(el);
+                });
 
-                dataArray.splice(lastIndex, 1); // remove the keyword 'user1' from the data Array
+                // myArray = myArray.filter(function (el) {
+                //   return !toRemove.includes(el);
+                // });
+                console.log(result);
+
+                let updateFile = result.join("\n");
+                fs.writeFile("text.txt", updateFile, (err) => {
+                  if (err) throw err;
+                  console.log("Successfully updated the file data");
+                });
+
+                // for (let index = 0; index < dataArray.length; index++) {
+                //   if (dataArray[index].includes(searchKeyword)) {
+                //     // check if a line contains the 'user1' keyword
+                //     dataArray.splice(lastIndex, 1); // remove the keyword 'user1' from the data Array
+                //     const updatedData = dataArray.join("\n");
+                //   }
+                // }
 
                 // UPDATE FILE WITH NEW DATA
                 // IN CASE YOU WANT TO UPDATE THE CONTENT IN YOUR FILE
                 // THIS WILL REMOVE THE LINE CONTAINS 'user1' IN YOUR shuffle.txt FILE
-                const updatedData = dataArray.join("\n");
-                fs.writeFile("text.txt", updatedData, (err) => {
-                  if (err) throw err;
-                  console.log("Successfully updated the file data");
-                });
               }
             );
           });
@@ -125,4 +133,28 @@ inquirer
       `https://create-scss.com`
     );
     copyFiles();
+
+    const saveFile = require("fs").writeFileSync;
+    const pkgJsonPath =
+      require.main.paths[0].split("node_modules")[0] + "package.json";
+    const json = require(pkgJsonPath);
+    console.log("check->" + pkgJsonPath);
+
+    if (!json.hasOwnProperty("scripts")) {
+      json.scripts = {};
+    }
+    json.scripts[
+      "cs-watch"
+    ] = `sass ${answer.scss_path}/scss/main.scss ${answer.scss_path}/css/style.css --watch --no-source-map`;
+    json.scripts[
+      "cs-compile"
+    ] = `sass ${answer.scss_path}scss/main.scss ${answer.scss_path}/css/style.css --no-source-map`;
+    json.scripts[
+      "cs-compress"
+    ] = `sass ${answer.scss_path}/scss/main.scss ${answer.scss_path}/css/style.css --style=compressed --no-source-map`;
+    json.scripts[
+      "cs-prefix"
+    ] = `postcss ${answer.scss_path}/css/style.css -o ${answer.scss_path}/css/style.css --use autoprefixer -b 'last 4 versions' --no-source-map`;
+    json.scripts["cs-build"] = "npm-run-all cs-compile cs-compress cs-prefix";
+    saveFile(pkgJsonPath, JSON.stringify(json, null, 2));
   });
